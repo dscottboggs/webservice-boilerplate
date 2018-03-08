41dc30ea1e49fa69e87661ff667d985409f37925
scott <scott@tams.tech>
2018-03-08 09:13:15 -0500

    restructured the for loops in the test to iterate over a hash instead.

--- test/controls/test-webserver.rb
+++ test/controls/test-webserver.rb
@@ -1,14 +1,21 @@
 # encoding: utf-8
 # copyright: 2018, D. Scott Boggs, Jr.
 
-title 'Configuration tests'
+require 'strings'
 
 home_dir = "/home/scott"
 service_names = ['test-webserver']
 service_urls = ['test']
+services = {
+  'test_webserver' => {
+    'url' => 'test',
+    'page' => test_webserver_page_200,
+    'redirectpage' => test_webserver_page_301
+  }
+}
 domain = 'tams.tech'
 docker_folder = "#{home_dir}/Documents/boilerplate/webservice/files/DockerVolumes"
-base_dir = "#{docker_folder}/#{service_name}"
+#base_dir = "#{docker_folder}/#{service_name}"
 proxy_dir = "#{docker_folder}/nginx-proxy"
 
 control 'manual_tests' do
@@ -24,22 +31,22 @@ control 'manual_tests' do
     it { should be_running }
     its('ports') { should cmp "0.0.0.0:80->80/tcp, 0.0.0.0:443->443/tcp"}
   end
-  for service in service_names do
-    describe docker_container(service) do
+  describe http('http://localhost/') do
+    its('status') { should cmp 503 }
+    its('body') { should cmp page_503 }
+  end
+  services.each do |svc_name, service|
+    describe docker_container(svc_name) do
       it { should exist }
       it { should be_running }
     end
-  end
-  describe http('http://localhost/') do
-    its('status') { should cmp 503 }
-  end
-  for url in service_urls do
     describe http("http://#{url}.#{domain}/") do
       its('status') { should cmp 301 }
+      its('body') { should cmp service['redirectpage'] }
     end
     describe http("https://#{url}.#{domain}/") do
       its('status') { should cmp 200 }
-      its('body') { should cmp nginx_default_index }
+      its('body') { should cmp service['page'] }
       its('headers.Content-Type') { should cmp 'text/html' }
     end
   end
