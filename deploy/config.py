bae23f0fed61e3721bb25dda01e7430a2ac5ae7f
scott <scott@tams.tech>
2018-03-09 15:39:23 -0500

    Moved the SecretStore back into config.py

--- deploy/config.py
+++ deploy/config.py
@@ -1,7 +1,6 @@
 import docker
 import logging
 import random
-from extras import SecretStore
 from logging.config import dictConfig
 from os.path import dirname as parent_dir_of
 from textwrap import dedent, wrap
@@ -45,6 +44,26 @@ loggingConfig = get_logging_config() or {
         "level": logging.DEBUG
     }
 }
+
+class SecretStore():
+    """A place to store and retrieve docker secrets."""
+    get_secret = lambda key: self.secrets[key]
+    def __init__(self):
+        self.secrets = {}
+        self._get_secrets = Config.DOCKER_CLIENT.secrets.list
+        self._create_secret = Config.DOCKER_CLIENT.secrets.create
+    def create_secret(self, key: str, value: str):
+        """Store a key-value pair as a docker secret."""
+        for secret in self._get_secrets():
+            if secret.name == key:
+                secret.remove()
+        self.secrets.update({
+            key: self._create_secret(name=key, data=value)
+        })
+    def create_secrets(self, secrets: dict):
+        """Do create_secret on a dict of secrets"""
+        for name, secret in secrets.values():
+            create_secret(name, secret)
 
 class Config():
     """Configuration items"""
--- deploy/extras.py
+++ deploy/extras.py
@@ -76,26 +76,6 @@ def check_images(images):
             else:
                 pull(repository=img)
 
-class SecretStore():
-    """A place to store and retrieve docker secrets."""
-    get_secret = lambda key: self.secrets[key]
-    def __init__(self):
-        self.secrets = {}
-        self._get_secrets = Config.DOCKER_CLIENT.secrets.list
-        self._create_secret = Config.DOCKER_CLIENT.secrets.create
-    def create_secret(self, key: str, value: str):
-        """Store a key-value pair as a docker secret."""
-        for secret in self._get_secrets():
-            if secret.name == key:
-                secret.remove()
-        self.secrets.update({
-            key: self._create_secret(name=key, data=value)
-        })
-    def create_secrets(self, secrets: dict):
-        """Do create_secret on a dict of secrets"""
-        for name, secret in secrets.values():
-            create_secret(name, secret)
-
 def _baseN(num,b,numerals="0123456789abcdefghijklmnopqrstuvwxyz"):
     """Convert a number to any base up to 36"""
     return ((num == 0) and numerals[0]) or (baseN(
